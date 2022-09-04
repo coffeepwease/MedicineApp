a06c154546b1c516cb8b1fbd406bb0ce5ac3b885
Lester Lim <lesterlim26082000@gmail.com>
2022-09-04 09:17:48 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+     <View style={styles.inputContainer}>
+      <TextInput style={styles.textInput} placeholder='Your Medical Goal!'/>
+      <Button title='Add Goal'/>
+     </View>
+    <View>
+      <Text>List of Goals...</Text>  
+    </View> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'Grey',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
+  }
+});
